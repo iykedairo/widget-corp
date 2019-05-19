846eacd4ea84d69b8de59aec4c5be5e942bef6c4
michaeliyke <ymichaelc@gmail.com>
2019-05-19 20:35:56 +0100

    Link navigation working!

--- content.php
+++ content.php
@@ -4,6 +4,17 @@ include_once "./includes/connection.php";
 require_once "./includes/functions.php";
 require_once "./includes/operatons.php";
 include_once "./includes/header.php";
+
+if (isset($_GET["subj"])) {
+    $selected_subj = $_GET["subj"];
+    $selected_page = "";
+} else if (isset($_GET["page"])) {
+    $selected_page = $_GET["page"];
+    $selected_subj = "";
+} else {
+    $selected_page = "";
+    $selected_subj = "";
+}
 ?>
 
     <table id="structure">
@@ -14,13 +25,15 @@ include_once "./includes/header.php";
       $subjects = retrieve("subjects", "*", " ORDER BY POSITION ASC", $connection);
 
       for_each( $subjects , function ($subject) {
-          echo "<li>{$subject['menu_name']}</li>";
+          $sub = urldecode($subject["id"]);
+          echo "<li> <a href='content.php?subj=$sub'> {$subject['menu_name']}</a></li>";
           global $connection;
           echo "<ul class='pages'>";
           $pages = retrieve("pages", " * ", "WHERE subject_id = {$subject['id']} ORDER BY POSITION ASC", $connection);
 
           for_each($pages, function ($page) {
-              echo "<li>{$page['menu_name']}</li>";
+              $pg = urldecode($page["id"]);
+              echo "<li> <a href='content.php?page=$pg'> {$page['menu_name']}</a></li>";
           });
           echo "</ul>";
       });
@@ -30,6 +43,11 @@ include_once "./includes/header.php";
       </td>
       <td id="page">
       <h2>Content area</h2>
+          <?php
+          echo $selected_subj;
+          echo "<br />";
+          echo $selected_page;
+          ?>
       </td>
       </tr>
     </table>
